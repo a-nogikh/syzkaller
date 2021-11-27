95a42e1915a7da21a16496a1951aa228198a4acc
Aleksandr Nogikh <nogikh@google.com>
2021-11-27 09:24:35 +0000

    syz-fuzzer: add 50ms delay between executions

--- syz-fuzzer/proc.go
+++ syz-fuzzer/proc.go
@@ -304,7 +304,7 @@ func (proc *Proc) executeRaw(opts *ipc.ExecOpts, p *prog.Prog, stat Stat) *ipc.P
 	// Limit concurrency window and do leak checking once in a while.
 	ticket := proc.fuzzer.gate.Enter()
 	defer proc.fuzzer.gate.Leave(ticket)
-
+	time.Sleep(time.Second / 20) // 50ms
 	proc.logProgram(opts, p)
 	for try := 0; ; try++ {
 		atomic.AddUint64(&proc.fuzzer.stats[stat], 1)
