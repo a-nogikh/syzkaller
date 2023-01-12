fbd766e5d586f5812f3c92498a8e3364c804ff36
Aleksandr Nogikh <nogikh@google.com>
2023-01-12 17:26:46 +0100

    pkg/testutil: set up directory layout
    Move the code from pkg/subsystem/match, as it can be useful
    project-wide.

--- pkg/subsystem/match/cover_test.go
+++ pkg/subsystem/match/cover_test.go
@@ -4,25 +4,21 @@
 package match
 
 import (
-	"path/filepath"
 	"testing"
 
-	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/pkg/testutil"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPathCoverQuerySubtree(t *testing.T) {
 	dir := t.TempDir()
 	// Create some dir hierarchy.
-	err := setupDirLayout(dir, []string{
+	testutil.DirectoryLayout(t, dir, []string{
 		"/a/b/c",
 		"/a/d/",
 		"/b/",
 		"/c",
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	subsystemA, subsystemB, subsystemC := "A", "B", "C"
 	matcher := func(path string) []interface{} {
@@ -54,23 +50,4 @@ func matchMapKeys(t *testing.T, m map[interface{}]struct{}, expected []interface
 		keys = append(keys, key)
 	}
 	assert.ElementsMatch(t, keys, expected)
-}
-
-func setupDirLayout(base string, paths []string) error {
-	for _, path := range paths {
-		path = filepath.Join(base, filepath.FromSlash(path))
-		dir := filepath.Dir(path)
-		// Create the directory.
-		err := osutil.MkdirAll(dir)
-		if err != nil {
-			return err
-		}
-		if path != "" && path[len(path)-1] != filepath.Separator {
-			err = osutil.WriteFile(path, nil)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
