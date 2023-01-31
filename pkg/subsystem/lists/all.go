c67b49584cf3cf3297a011c715c5471ef5005b0f
Aleksandr Nogikh <nogikh@google.com>
2023-01-31 14:05:27 +0100

    pkg/subsystem: store linux_upstream subsystems
    Also add a few basic sanity tests.

---
+++ pkg/subsystem/lists/all.go
@@ -0,0 +1,9 @@
+// Copyright 2023 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package lists
+
+import (
+	// Import all lists.
+	_ "github.com/google/syzkaller/pkg/subsystem/lists/linux_upstream"
+)
