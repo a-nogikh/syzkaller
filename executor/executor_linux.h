41c41e0456626b3621b31bfe375de33d845bc411
Aleksandr Nogikh <nogikh@google.com>
2026-01-29 15:05:55 +0000

    executor: add tests for cmdline extraction

--- executor/executor_linux.h
+++ executor/executor_linux.h
@@ -366,25 +366,6 @@ static const char* setup_kcov_reset_ioctl()
 	return error;
 }
 
-static void get_last_opt(const char* cmdline, const char* key, char* out, size_t out_len)
-{
-	char key_eq[128];
-	snprintf(key_eq, sizeof(key_eq), "%s=", key);
-	const char* val = NULL;
-	for (const char* p = cmdline; (p = strstr(p, key_eq)); p++) {
-		if (p == cmdline || p[-1] == ' ' || p[-1] == '\t' || p[-1] == '\n')
-			val = p + strlen(key_eq);
-	}
-
-	if (val) {
-		size_t len = strcspn(val, " \t\n");
-		if (len >= out_len)
-			len = out_len - 1;
-		memcpy(out, val, len);
-		out[len] = 0;
-	}
-}
-
 static const char* setup_kdump()
 {
 	if (access("/boot/bzImageKexec", F_OK) != 0)
